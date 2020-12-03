44fbd12a26c3f64c3e8cef66a9d631467dfd3e39
Shehan Wijethunga <pwshehan@gmail.com>
2020-12-03 07:14:11 +0530

    Loading HTML Docs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const { BrowserWindow } = require("electron");
 const electron = require("electron");
 
 const { app } = electron;
 
 app.on("ready", () => {
-  console.log("App is ready!");
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
