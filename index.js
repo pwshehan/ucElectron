ea0f28b914f60880f5ae40239d39906fe3d47af9
Shehan Pramitha <pwshehan@gmail.com>
2020-12-20 20:04:05 +0530

    Receiving IPC Events

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const ffmpeg = require("fluent-ffmpeg");
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: { nodeIntegration: true },
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on("video:submit", (event, path) => {
-  console.log(path);
   ffmpeg.setFfprobePath("C:\\src\\ffmpeg\\bin\\ffprobe.exe");
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log("File duration is: ", metadata.format.duration);
+    mainWindow.webContents.send("video:metadata", metadata.format.duration);
   });
 });
