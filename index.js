2e19f5f5e7fb49e42cfa77374ac5b7e31bb90f6a
Shehan Pramitha <pwshehan@gmail.com>
2020-12-20 19:48:42 +0530

    Video Duration with FFProbe

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const { BrowserWindow } = require("electron");
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
-const { app } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on("video:submit", (event, path) => {
+  console.log(path);
+  ffmpeg.setFfprobePath("C:\\src\\ffmpeg\\bin\\ffprobe.exe");
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log("File duration is: ", metadata.format.duration);
+  });
 });
