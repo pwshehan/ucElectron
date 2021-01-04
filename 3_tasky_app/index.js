87a4d3ef77d7a93322b772b6003b3f8939b46ef9
Shehan Pramitha <pwshehan@gmail.com>
2021-01-04 11:10:47 +0530

    show Browser window by try icon

--- 3_tasky_app/index.js
+++ 3_tasky_app/index.js
@@ -4,6 +4,7 @@ const electron = require("electron");
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
@@ -12,11 +13,16 @@ app.on("ready", () => {
     width: 300,
     frame: false,
     resizable: false,
+    show: false,
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
   const iconName =
     process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new Tray(iconPath);
+  tray = new Tray(iconPath);
+  tray.on("click", () => {
+    // if()
+    mainWindow.show();
+  });
 });
