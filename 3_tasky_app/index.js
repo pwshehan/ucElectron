ec0d1997303df7912d410b6c8deffb742f50d6b3
Shehan Pramitha <pwshehan@gmail.com>
2021-01-04 10:42:30 +0530

    bug fix

---
+++ 3_tasky_app/index.js
@@ -0,0 +1,12 @@
+const electron = require("electron");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
